a3ed7cdea92a38f3fa88f94c3c28120dfd88669a
Ewald Stieger <e.stieger@gmail.com>
2014-06-18 12:22:19 +0200

    Add more files

---
+++ WebApiProxyGen/Handlers/MetadataHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiProxyGen.Metadata;
+
+namespace WebApiProxyGen.Handlers
+{
+    public class MetadataHandler : DelegatingHandler
+    {
+
+        private MetadataProvider _metadataProvider;
+        public MetadataHandler()
+        {
+            _metadataProvider = new MetadataProvider();
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+
+
+
+            return await Task.Run(() =>
+            {
+
+                var metadata = _metadataProvider.GetMetadata(request);
+
+                //if (request.Headers.Any(h => h.Key == "X-Proxy-Type" && h.Value.Contains("metadata")))
+                return request.CreateResponse(System.Net.HttpStatusCode.OK, metadata);
+
+
+                //var template = new JsProxyTemplate(metadata);
+                //var js = new StringContent(template.TransformText());
+
+                //js.Headers.ContentType = new MediaTypeHeaderValue("application/javascript");
+
+                //return new HttpResponseMessage { Content = js }; ;
+
+
+
+
+
+            });
+
+        }
+
+
+
+
+    }
+}
